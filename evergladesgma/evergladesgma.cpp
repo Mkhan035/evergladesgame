24c52808052d014143729e92e4ebdc8f02aa4e4d
Mkhan035 <150642877+Mkhan035@users.noreply.github.com>
2023-11-28 11:53:30 -0500

    Update evergladesgma.cpp

--- evergladesgma/evergladesgma.cpp
+++ evergladesgma/evergladesgma.cpp
@@ -50,58 +50,161 @@ Ranger moves first; Options after
 
 // random ints, iomanip, 3 function prototypes
 #include <iostream>
-#include <Random>
-#include <iomanip>
 #include <string>
+#include <random>
 using namespace std;
-
-//Function Prototypes
-
-void showBoard(int[][5]);
-void showRules();
-int validMenuOption(int);
-
+int randomNum(int, int);
+void showBoard(string[][5]);
+string dangerIdentified(string[][5], int&, bool&);
 int main()
 {
-    const int ROWS = 5, COLS = 5;
-    int board[ROWS][COLS];
+    string displayBoard[5][5];
+    const int rows = 5, cols = 5;
+    string board[5][5] = {
+        {"R", "D", "S", "S", "D"},
+        {"D","S", "S", "S", "D"},
+        {"S", "D", "S", "S", "D"},
+        {"S","D", "S", "D", "S"},
+        {"S","D","S","D","T"}
+    };
     bool playing = true;
+    int Rx = 0, Ry = 0;
+    int initial_x = 0, initial_y = 0;
+    bool advance = true;
+    int gongs = 12;
     int menu_option;
-
+    
+    
+    
     do {
 
-        cout << "Lost in the Everglades ..." << endl << endl;
-        cout << "Let's play! Review the menu options below." << endl;
-        cout << "1. Game Rules" << endl
-            << "2. Play Game" << endl
-            << "3. Quit" << endl << endl;
-        cout << "Please select a menu option: ";
+        cout << "\nLost in the Everglades ..." << endl << endl;
+        cout << "\nLet's play! Pick a menu option: ";
         cin >> menu_option;
 
         switch (menu_option) {
         case 1:
-            showRules();
+           
             break;
         case 2:
+            for (int i = 0; i < 5; ++i) {
+                for (int j = 0; j < 5; ++j) {
+                    displayBoard[i][j] = '*';
+                    displayBoard[Rx][Ry] = 'R';
+                    displayBoard[4][4] = 'T';
+
+                }
+            }
+            while (gongs > 0) {
+                showBoard(displayBoard);
+                cout << "Enter next cell (row & col): ";
+                cin >> Rx >> Ry;
+                cout << endl << endl;
+
+                while ((Rx < 0 || Rx > 4) || (Ry > 4 || Ry < 0)) {
+                    cout << "Bad ... Try again" << endl;
+                    cout << "enter next cell (row & col): ";
+                    cin >> Rx >> Ry;
+                }
+
+             
+
+                if (board[Rx][Ry] == "D") {
+                    cout << "\nChecking for danger ...";
+                    displayBoard[Rx][Ry] = board[Rx][Ry] = dangerIdentified(board, gongs, advance);
+                    if (advance == true) {
+                        cout << "\nAdvance is true";
+                       
+                        displayBoard[initial_x][initial_y] = " ";
+                        board[initial_x][initial_y] = "O";
+                        initial_x = Rx; initial_y = Ry;
+                        displayBoard[Rx][Ry] = "R";
+                    }
+                    else {
+                        displayBoard[initial_x][initial_y] = "R";
+                    }
+                }
+                else {
+                    displayBoard[initial_x][initial_y] = " ";
+                    board[initial_x][initial_y] = "O";
+                    initial_x = Rx; initial_y = Ry;
+                    displayBoard[Rx][Ry] = "R";
+                    gongs -= 1;
+                }
+                cout << "\nGongs left: " << gongs << endl;
+            }
             break;
-
-
         case 3:
+            cout << "\nBye bye!";
+            playing = false;
             break;
         default:
             cout << "Invalid menu option.";
         }
-    } while (menu_option != 3);
+    } while (playing == true);
+    return 0;
+}
+    
+int randomNum(int x, int y) {
+    int value;
+    random_device myEngine;
+    uniform_int_distribution<int>randomInt(x, y);
+    value = randomInt(myEngine);
+    return value;
+}
+string dangerIdentified(string board[][5], int& gongs, bool& advance) {
+    string possibleDangers[4] = { "Famished Alligator", "Swarm of Mosquitos", "Venomous Spider", "Python" };
+    string symbolBoard[4] = { "A", "M", "S", "P" };
+    string board_mark = "N";
+    int rDanger, choice, winOrLose;
+    rDanger = randomNum(0, 3);
+    board_mark = symbolBoard[rDanger];
+    cout << "\nBegin danger execution";
+    cout << "\nUh oh, you've encountered a " << possibleDangers[rDanger] << "!";
+    cout << "\nChoose your next move: " << endl;
+    cout << "\t1. Wait until it leaves" << endl;
+    cout << "\t2. Fight it" << endl;
+    cout << "\nMove: ";
+    cin >> choice;
+
+    while (choice < 1 || choice >2) {
+        cout << "\nPick option 1 or two---there are no alternatives";
+        cout << "\nMove: ";
+        cin >> choice;
+    }
+    if (choice == 1) {
+        cout << "\n--->" << possibleDangers[rDanger] << " is gone! You advance!";
+        gongs -= 5;
+    }
+    if (choice == 2) {
+        winOrLose = randomNum(0, 1);
+        if (winOrLose == 0) {
+            cout << "\n--->You fight the " << possibleDangers[rDanger] << " and win! Go forth!";
+            gongs -= 2;
+            advance = true;
+        }
+        if (winOrLose == 1) {
+            cout << "\n--->Uh oh, you lost! Retreat!";
+            gongs -= 3;
+            advance = false;
+        }
 
     }
-
-void showRules()
-{
-    cout << "Please read the rules below." << endl << endl;
-    cout << "1. Player begins with 12 gongs of time; Game ends once gongs reaches 0."
-        << endl << "2. Ranger must move one cell at a time; Loses a gong per turn unless danger encountered." 
-        << endl << "3. If encountering danger, ranger must: Wait or Fight." 
-        << endl << "4. If Ranger waits: Moves to desired cell, loses 5 gongs of time, danger remains in cell."
-        << endl << "5. If Ranger fights and wins: Moves to desired cell, loses 2 gongs of time."
-        << endl << "6. If Ranger fights and loses: Goes back to initial cell, loses 3 gongs of time." << endl << endl;
+    return board_mark;
+}
+void showBoard(string displayBoard[5][5]) {
+    cout << "     0   1   2   3   4" << endl;
+    for (int i = 0; i < 5; i++) {
+        cout << i << " ";
+        if (i < 5) {
+            cout << " | ";
+        }
+        for (int j = 0; j < 5; j++) {
+            cout << displayBoard[i][j];
+            if (j < 5) {
+                cout << " | ";
+            }
+        }
+        cout << '\n';
+    }
 }
